5275db4998ac33182f86a0dedc614143eb4a481b
dmitryagoshkov <agodimon@mail.ru>
2022-07-06 20:37:48 +0500

    реализовал RecyclerView на homework

--- app/src/main/java/com/example/studentdiary/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/studentdiary/ui/home/HomeFragment.kt
@@ -1,36 +1,32 @@
 package com.example.studentdiary.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Adapter
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studentdiary.R
-import com.example.studentdiary.data.model.HomeWork
-import com.example.studentdiary.data.source.HomeworkSource
-import com.example.studentdiary.databinding.FragmentHomeBinding
+import com.example.studentdiary.ui.home.adapters.ClassesAdapter
+import com.example.studentdiary.ui.home.adapters.HomeworkAdapter
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
-    private var recyclerView: RecyclerView? = null
+    private var recyclerViewClasses: RecyclerView? = null
+    private var recyclerViewHomework: RecyclerView? = null
     private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<HomeWorkAdapter.HomeWorkViewHolder>? = null
+    private var adapterClasses: RecyclerView.Adapter<ClassesAdapter.HomeWorkViewHolder>? = null
+    private var adapterHomework: RecyclerView.Adapter<HomeworkAdapter.HomeWorkViewHolder>? = null
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         layoutManager = LinearLayoutManager(null)
-        adapter = HomeWorkAdapter()
-
-        recyclerView = view.findViewById(R.id.classes_recycle_view)
-        recyclerView?.adapter = adapter
-
+        adapterClasses = ClassesAdapter()
+        adapterHomework = HomeworkAdapter()
+        recyclerViewClasses = view.findViewById(R.id.classes_recycle_view)
+        recyclerViewClasses?.adapter = adapterClasses
+        recyclerViewHomework = view.findViewById(R.id.rv_homeworks)
+        recyclerViewHomework?.adapter = adapterHomework
     }
 
 }
--- app/src/main/java/com/example/studentdiary/ui/home/adapters/ClassesAdapter.kt
+++ app/src/main/java/com/example/studentdiary/ui/home/adapters/ClassesAdapter.kt
@@ -1,21 +1,15 @@
-package com.example.studentdiary.ui.home
+package com.example.studentdiary.ui.home.adapters
 
-import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.studentdiary.R
 
-import com.example.studentdiary.data.model.HomeWork
-import com.example.studentdiary.data.source.HomeworkSource
-import com.example.studentdiary.databinding.ItemHomeworkBinding
-
-class HomeWorkAdapter :
-    RecyclerView.Adapter<HomeWorkAdapter.HomeWorkViewHolder>() {
+class ClassesAdapter :
+    RecyclerView.Adapter<ClassesAdapter.HomeWorkViewHolder>() {
 
     private var imageLessons = listOf(R.drawable.ic_ball_foreground,R.drawable.ic_books_foreground,R.drawable.ic_onion_foreground)
     private var textTitle = listOf("Basketball","Read literature","Strike")
@@ -32,7 +26,7 @@ class HomeWorkAdapter :
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): HomeWorkAdapter.HomeWorkViewHolder {
+    ): HomeWorkViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_class, parent, false)
         return HomeWorkViewHolder(view)
